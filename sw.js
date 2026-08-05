7219c292148dadf2e91950c66fb0ea9a8982125e
SESCOL <ileseur78@gmail.com>
2026-08-05 11:00:56 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Service worker que no cachea - siempre red primero
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
